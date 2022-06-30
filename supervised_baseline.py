5a89265ffd42cac710aecf89a9c6a382cf9cb758
FardinAhsan146 <fardinahsan146@gmail.com>
2022-06-30 19:39:27 +0400

    baseline

--- supervised_baseline.py
+++ supervised_baseline.py
@@ -1,9 +1,13 @@
-import os
+import re
 import pandas as pd
 import numpy as np
-from collections import namedtuple
+from sklearn.preprocessing import MinMaxScaler
 
 """
+WARNING: If you just downloaded the Elliptic dataset and placed it in the data directory
+RUN THE data_cleaning.ipynb once before proceeding, It modifies the files. 
+Everything else will be self contained after that. 
+
 This file is basically testing the simple supervised models
 in order to get a baseline idea of how the models perform with minimal preprocessing
 
@@ -15,23 +19,38 @@ And building some functions that can be used later
 """
 
 def read_data():
-    """
+    """    
     Read the two csvs (classes and features)
     Edges will not be used for non graph based models
     """
-    raise NotImplementedError
+    df_classes = pd.read_csv("data/elliptic_txs_classes.csv")
+    df_features = pd.read_csv("data/elliptic_txs_features.csv")
     
-def preprocess_data(nodes,classes):
+    return df_classes,df_features
+    
+def preprocess_data(classes,features):
     """
     Cleans the data and preprocesses as required
     """
-    raise NotImplementedError
+    # Merge two datasets
+    df = pd.merge(classes, features)
     
+    # Getrid of unknown columns
+    df = df.loc[(df['class'] != 'unknown')] 
+    
+    # Turning class into is_illicit
+    df['class'] = df['class'].apply(lambda row: 1 if (row == 'illicit') else 0)
+    df = df.rename(columns={'class': 'is_illicit'})
+    
+    return df
+
 def feature_engineer(node_classes):
     """
     Going to apply standard normalization
     """
-    raise NotImplementedError
+    # Normalize df
+    transformed = MinMaxScaler().fit_transform(node_classes.values)
+    return pd.DataFrame(transformed,columns = node_classes.columns)
     
 def temporal_test_split(node_classes):
     """
@@ -40,28 +59,31 @@ def temporal_test_split(node_classes):
     The exact same split will be used other. 
     
     """
-    raise NotImplementedError
+    pass
+
     
 def train_and_evaluate(model):
     """
     Pass in a model object
-    Use default parameters
+    Use default hyperparameters
+    
+    Will use 10fold CV later
     """
-    raise NotImplementedError
+    pass
     
 if __name__ == "__main__":
+    
+    df_classes,df_features = read_data()
 
-    nodes_df,classes_df = read_data()
-
-    df = preprocess_data(nodes_df,classes_df)
+    df = preprocess_data(df_classes,df_features)
     
     df = feature_engineer(df)
     
-    train_X,test_X,train_Y,test_Y = temporal_test_split(df)
+    # train_X,test_X,train_Y,test_Y = temporal_test_split(df)
     
-    models = []
+    # models = []
     
-    for model in models:
-        train_and_evaluate(model)
+    # for model in models:
+    #     train_and_evaluate(model)
         
     
