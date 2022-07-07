4a8f424511236f8402cea3e1987d727a1260aaf4
FardinAhsan146 <fardinahsan146@gmail.com>
2022-07-07 20:30:54 +0400

    custom

--- src/customloss_rf_test.py
+++ src/customloss_rf_test.py
@@ -1,15 +1,41 @@
 import supervised_baseline as util 
-
-
+from sklearn.metrics import f1_score
+from sklearn.ensemble import RandomForestClassifier
 """
-Going to extend random forest to use 
+Going to extend/modify random forest to use 
 custom loss functions 
 
 """
 
-def main():
-    pass
-
 
 if __name__ == '__main__':
-    pass
+        
+
+    df = pd.read_csv('../data/normalized.csv')
+    
+    X_train, X_test, Y_train, Y_test = util.temporal_test_split(df)
+    
+    fit_model,Y_pred = util.train(X_train,Y_train,X_test, model = RandomForestClassifier())
+        
+    util.evaluate(Y_test,Y_pred)
+        
+
+"""
+# Deafult loss function
+Printing classification report for RandomForestClassifier 
+
+              precision    recall  f1-score   support
+
+         0.0       0.98      1.00      0.99     13091
+         1.0       0.99      0.65      0.79       879
+
+    accuracy                           0.98     13970
+   macro avg       0.98      0.83      0.89     13970
+weighted avg       0.98      0.98      0.98     13970
+
+#recall loss function
+
+
+
+
+"""
